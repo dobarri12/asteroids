289f1951cff895c4f4dae36cd40d75454b871e2d
dbarri12 <danielbarrington48@gmail.com>
2024-10-03 10:38:36 -0700

    Step 2: Adding Triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
 	pygame.init()
@@ -8,6 +9,10 @@ def main():
 	print(f"Screen width: {SCREEN_WIDTH}")
 	print(f"Screen height: {SCREEN_HEIGHT}")
 	
+	dt = 0
+	clock = pygame.time.Clock()
+
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	BLACK = (0, 0, 0)
 
 	while True:
@@ -15,8 +20,10 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
-
+		
+		player.draw(screen)
 		pygame.display.flip()
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
 	main()
